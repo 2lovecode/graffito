d8008074feb3a54c1defd0bcf1f9bf0adc5f532d
2lovecode <liu546hao@163.com>
2020-02-25 19:41:05 +0800

    edit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module graffito
+
+go 1.13
+
+require (
+	github.com/garyburd/redigo v1.6.0 // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	github.com/willf/bitset v1.1.10 // indirect
+	github.com/willf/bloom v2.0.3+incompatible // indirect
+)
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+type params struct {
+	help bool
+	module string
+	task string
+}
+
+var p params
+
+func init() {
+
+	flag.BoolVar(&p.help, "h",false, "帮助")
+	flag.StringVar(&p.module,"m", "tools", "指定模块名")
+	flag.StringVar(&p.task,"t", "hello", "指定选定模块下需要执行的任务名")
+
+	flag.Usage = usage
+}
+
+func main() {
+	flag.Parse()
+	if p.help {
+		flag.Usage()
+	}
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, `graffito version: 0.0.0
+用法: go run main.go [-m module] [-t task] [-h]
+
+选项:
+
+`)
+	flag.PrintDefaults()
+}
