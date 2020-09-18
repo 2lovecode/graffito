3f6f8544dca92defddb9da6df255cd00f6b1074d
2lovecode <liu546hao@163.com>
2020-09-18 18:36:41 +0800

    docs:doc

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main
 
+import (
+	"os"
+	"runtime/trace"
+)
+
 func main() {
+	// 代码是无用的
+	// https://www.cnblogs.com/nickchen121/p/11517452.html
+	trace.Start(os.Stderr)
+
+	defer trace.Stop()
+
+	ch := make(chan string)
+
+	go func() {
+		ch <- "hello world"
+	}()
 }
 
 
