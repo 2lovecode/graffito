950745f0e3ab473c0d4342a9c8a15c93f3529297
2lovecode <liu546hao@163.com>
2021-04-21 17:13:15 +0800

    50%

--- algorithm/mbtree.go
+++ algorithm/mbtree.go
@@ -13,6 +13,19 @@ func (mn *mBTreeNode) insert(value int) {
 
 }
 
+// 节点分裂
+func (mn *mBTreeNode) split(i int) (value int, next *mBTreeNode){
+	value = mn.items[i]
+	next = newMBTreeNode()
+	next.items = append(next.items, mn.items[i+1:]...)
+	mn.items = mn.items[:i]
+	if len(mn.children) > 0 {
+		next.children = append(next.children, mn.children[i+1:]...)
+		mn.children = mn.children[:i]
+	}
+	return
+}
+
 
 
 type MBTree struct {
@@ -30,9 +43,21 @@ func NewMBTree() *MBTree {
 
 func (mt *MBTree) Insert(value int) {
 	if mt.root == nil {
+		mt.root = newMBTreeNode()
+		mt.root.items = append(mt.root.items, value)
+	} else {
+		if len(mt.root.items) >= (mt.num - 1) {
+			// 根节点满了，需要分裂
+			newRootValue, rightNode := mt.root.split((mt.num - 1) / 2)
+			leftNode := mt.root
 
+			newRootNode := newMBTreeNode()
+			newRootNode.items = append(newRootNode.items, newRootValue)
+			newRootNode.children = append(newRootNode.children, leftNode, rightNode)
+			mt.root = newRootNode
+		}
+		mt.root.insert(value)
 	}
-	mt.root.insert(value)
 }
 
 
