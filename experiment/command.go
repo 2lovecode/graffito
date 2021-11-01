67486903f33211cbf39a890ff3093f2be8bb2e71
2lovecode <liu546hao@163.com>
2021-11-01 17:19:31 +0800

    edit

--- experiment/command.go
+++ experiment/command.go
@@ -1,7 +1,7 @@
 package experiment
 
 import (
-	"graffito/experiment/g3n"
+	"graffito/experiment/cache"
 
 	"github.com/spf13/cobra"
 )
@@ -10,11 +10,16 @@ func NewExperimentCommand() *cobra.Command {
 
 	expCmd := &cobra.Command{Use: "exp", Short: "试验代码"}
 
-	stringOpCmd := &cobra.Command{Use: "g3n", Run: func(cmd *cobra.Command, args []string) {
-		g3n.Run()
-	}, Short: "g3n游戏包示例代码"}
+	// stringOpCmd := &cobra.Command{Use: "g3n", Run: func(cmd *cobra.Command, args []string) {
+	// 	g3n.Run()
+	// }, Short: "g3n游戏包示例代码"}
 
-	expCmd.AddCommand(stringOpCmd)
+	// expCmd.AddCommand(stringOpCmd)
+	cacheCmd := &cobra.Command{Use: "cache", Run: func(cmd *cobra.Command, args []string) {
+		cache.Run()
+	}, Short: "3类缓存问题"}
+
+	expCmd.AddCommand(cacheCmd)
 
 	return expCmd
 }
--- practice/command.go
+++ practice/command.go
@@ -1,6 +1,8 @@
 package practice
 
 import (
+	"graffito/practice/slice_x"
+
 	"github.com/spf13/cobra"
 )
 
@@ -8,11 +10,11 @@ func NewPracticeCommand() *cobra.Command {
 
 	pracCmd := &cobra.Command{Use: "prac", Short: "练习代码"}
 
-	// stringOpCmd := &cobra.Command{Use: "g3n", Run: func(cmd *cobra.Command, args []string) {
-	// 	g3n.Run()
-	// }, Short: "g3n游戏包示例代码"}
+	slice1OpCmd := &cobra.Command{Use: "slice_1", Run: func(cmd *cobra.Command, args []string) {
+		slice_x.Run_1()
+	}}
 
-	// pracCmd.AddCommand(stringOpCmd)
+	pracCmd.AddCommand(slice1OpCmd)
 
 	return pracCmd
 }
