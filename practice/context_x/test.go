13c527f0b0fc88509de869ff1eef53b388774ba5
2lovecode <liu546hao@163.com>
2023-02-09 17:05:53 +0800

    add learn readme

--- practice/context_x/test.go
+++ practice/context_x/test.go
@@ -2,33 +2,38 @@ package context_x
 
 import (
 	"context"
+	"fmt"
 	"time"
 )
 
-func Run1() (x1 string, x2 string){
-	ctx, _ := context.WithTimeout(context.Background(), 50 * time.Millisecond)
+func Run1() (x1 string, x2 string) {
+	ctx, _ := context.WithTimeout(context.Background(), 50*time.Millisecond)
 	ch := make(chan bool)
 
-	go func () {
+	go func() {
 		select {
-		case <- ctx.Done():
+		case <-ctx.Done():
 			x1 = "done"
-		case <- ch:
+		case <-ch:
 			x1 = "ok"
 		}
 	}()
 
 	go func() {
 		select {
-		case <- ctx.Done():
+		case <-ctx.Done():
 			x2 = "done"
-		case <- ch:
+		case <-ch:
 			x2 = "ok"
 		}
 	}()
 
-	<- ctx.Done()
+	<-ctx.Done()
 	time.Sleep(200 * time.Millisecond)
 
 	return x1, x2
+}
+
+func Run2() {
+	fmt.Println("Good Morning!")
 }
