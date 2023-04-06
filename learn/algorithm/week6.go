264d2aae8cdedce35aac3995037c8a5ec7be797b
2lovecode <liu546hao@163.com>
2023-04-06 21:04:03 +0800

    a

---
+++ learn/algorithm/week6.go
@@ -0,0 +1,16 @@
+package algorithm
+
+import "fmt"
+
+// 二叉树反转
+// 判断是否是平衡二叉树
+// 判断是否是对称二叉树
+
+func JudgeBalanceBinaryTree() {
+	fmt.Println("Hello")
+
+}
+
+func JudgeSymmetricalBinaryTree() {
+	fmt.Println("World")
+}
