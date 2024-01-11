ce62373e3e4eb918bd037e313d1c68de0d12f92a
2lovecode <liu546hao@163.com>
2024-01-11 20:45:28 +0800

    leetcode-climb stairs

--- leetcode/command.go
+++ leetcode/command.go
@@ -1,8 +1,32 @@
 package leetcode
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/spf13/cobra"
+	"graffito/leetcode/dynamic_programming/fibonacci"
+)
 
 func NewCommand() *cobra.Command {
 	leetCmd := &cobra.Command{Use: "leet", Short: "leetcode题解"}
+
+	var stairsCount int
+
+	cmds := []*cobra.Command{
+		{
+			Use:   "climb_stairs",
+			Short: "爬楼梯",
+			Run: func(cmd *cobra.Command, args []string) {
+				fibonacci.TestClimbStairs(stairsCount)
+			},
+		},
+	}
+	// 设置 "climb_stairs" 子命令的参数
+	for _, cmd := range cmds {
+		switch cmd.Use {
+		case "climb_stairs":
+			cmd.Flags().IntVar(&stairsCount, "count", 1, "楼梯总数")
+		}
+	}
+
+	leetCmd.AddCommand(cmds...)
 	return leetCmd
 }
---
+++ leetcode/dynamic_programming/fibonacci/climb_stairs.go
@@ -0,0 +1,63 @@
+package fibonacci
+
+import "fmt"
+
+//70. 爬楼梯
+//简单
+//相关标签
+//相关企业
+//提示
+//
+//假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+//
+//每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+//
+//
+//
+//示例 1：
+//
+//输入：n = 2
+//输出：2
+//解释：有两种方法可以爬到楼顶。
+//1. 1 阶 + 1 阶
+//2. 2 阶
+//
+//示例 2：
+//
+//输入：n = 3
+//输出：3
+//解释：有三种方法可以爬到楼顶。
+//1. 1 阶 + 1 阶 + 1 阶
+//2. 1 阶 + 2 阶
+//3. 2 阶 + 1 阶
+//
+//
+//
+//提示：
+//
+//1 <= n <= 45
+
+var climbStairsMap = make(map[int]int)
+
+func TestClimbStairs(n int) {
+	fmt.Println("爬楼种数：", climbStairs(n))
+}
+
+func climbStairs(n int) int {
+	y := 0
+	if x, ok := climbStairsMap[n]; ok {
+		y = x
+	} else {
+		switch n {
+		case 1:
+			y = 1
+		case 2:
+			y = 2
+		default:
+			y = climbStairs(n-2) + climbStairs(n-1)
+		}
+		climbStairsMap[n] = y
+	}
+
+	return y
+}
