5e3c112535535f9090a6d21d416f4aefe0f47369
2lovecode <liu546hao@163.com>
2024-03-29 18:14:10 +0800

    sql2go

--- tools/sql2go/main.go
+++ tools/sql2go/main.go
@@ -1,13 +1,13 @@
 package sql2go
 
 import (
+	"fmt"
+
 	"github.com/qmhball/db2gorm/gen"
 	"github.com/spf13/cobra"
 )
 
-func Run() {
-	dsn := "root:user237@tcp(10.10.10.237:3306)/mydb?charset=utf8&parseTime=true&loc=Local"
-
+func Run(dsn string) {
 	//生成指定单表
 	tblName := "User"
 	gen.GenerateOne(gen.GenConf{
@@ -19,7 +19,19 @@ func Run() {
 }
 
 func NewCommand() *cobra.Command {
-	return &cobra.Command{Use: "sql2go", Run: func(cmd *cobra.Command, args []string) {
-		Run()
+	user := ""
+	pass := ""
+	addr := ""
+	dbname := ""
+	cmd := &cobra.Command{Use: "sql2go", Run: func(cmd *cobra.Command, args []string) {
+		dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=true&loc=Local", user, pass, addr, dbname)
+		Run(dsn)
 	}, Short: "sql2go", Example: "graffito tools sql2go"}
+
+	cmd.Flags().StringVarP(&user, "user", "u", "", "用户名")
+	cmd.Flags().StringVarP(&pass, "pass", "p", "", "密码")
+	cmd.Flags().StringVarP(&addr, "addr", "a", "", "Addr")
+	cmd.Flags().StringVarP(&dbname, "dbname", "d", "", "数据库")
+
+	return cmd
 }
