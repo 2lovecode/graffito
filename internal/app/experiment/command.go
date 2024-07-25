41e206de3c3eff5f2cd9bceb2e0e596e163e67f4
2lovecode <liu546hao@163.com>
2024-07-25 22:50:03 +0800

    code convert

--- internal/app/experiment/command.go
+++ internal/app/experiment/command.go
@@ -1,8 +1,14 @@
 package experiment
 
 import (
+	"bytes"
 	"context"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"time"
+
 	"github.com/2lovecode/graffito/internal/app/experiment/concx"
 	"github.com/2lovecode/graffito/internal/app/experiment/depends"
 	"github.com/2lovecode/graffito/internal/app/experiment/event"
@@ -12,7 +18,10 @@ import (
 	"github.com/2lovecode/graffito/internal/app/experiment/mode0/source"
 	"github.com/2lovecode/graffito/pkg/cache"
 	"github.com/2lovecode/graffito/pkg/params"
-	"time"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/ianaindex"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
 
 	"github.com/spf13/cobra"
 )
@@ -96,6 +105,26 @@ func NewCommand() *cobra.Command {
 		fmt.Println(sAData, sBData, sCData)
 	}, Short: "depends"}
 
+	pingCmd := &cobra.Command{
+		Use: "ping",
+		Run: func(cmd *cobra.Command, args []string) {
+			p := exec.Command("powershell", "-Command", "ping", "www.baidu.com")
+
+			var out bytes.Buffer
+
+			p.Stdout = &out
+
+			_ = p.Run()
+
+			// encoding, name, certain := charset.DetermineEncoding(out.Bytes(), "")
+			// fmt.Printf("编码：%v\n名称：%s\n确定：%t\n", encoding, name, certain)
+
+			output, _ := GbkToUtf8(out.Bytes())
+			fmt.Println("Out: ", string(output))
+
+		},
+	}
+
 	expCmd := &cobra.Command{Use: "exp", Short: "试验代码"}
 
 	cmds := []*cobra.Command{
@@ -103,9 +132,121 @@ func NewCommand() *cobra.Command {
 		dependsCmd,
 		mode0Cmd,
 		eventCmd,
+		pingCmd,
 		concx.NewCommand(),
 	}
 
 	expCmd.AddCommand(cmds...)
 	return expCmd
+}
+
+type Charset string
+
+// 中文
+const (
+	GBK     Charset = "GBK"
+	GB18030         = "GB18030"
+	GB2312          = "GB2312"
+	Big5            = "Big5"
+)
+
+// 日文
+const (
+	EUCJP     Charset = "EUCJP"
+	ISO2022JP         = "ISO2022JP"
+	ShiftJIS          = "ShiftJIS"
+)
+
+// 韩文
+const (
+	EUCKR Charset = "EUCKR"
+)
+
+// Unicode
+const (
+	UTF_8    Charset = "UTF-8"
+	UTF_16           = "UTF-16"
+	UTF_16BE         = "UTF-16BE"
+	UTF_16LE         = "UTF-16LE"
+)
+
+// 其他编码
+const (
+	Macintosh Charset = "macintosh"
+	IBM               = "IBM*"
+	Windows           = "Windows*"
+	ISO               = "ISO-*"
+)
+
+var charsetAlias = map[string]string{
+	"HZGB2312": "HZ-GB-2312",
+	"hzgb2312": "HZ-GB-2312",
+	"GB2312":   "HZ-GB-2312",
+	"gb2312":   "HZ-GB-2312",
+}
+
+func Convert(dstCharset Charset, srcCharset Charset, src string) (dst string, err error) {
+	if dstCharset == srcCharset {
+		return src, nil
+	}
+	dst = src
+	// Converting <src> to UTF-8.
+	if srcCharset != "UTF-8" {
+		if e := getEncoding(srcCharset); e != nil {
+			tmp, err := ioutil.ReadAll(
+				transform.NewReader(bytes.NewReader([]byte(src)), e.NewDecoder()),
+			)
+			if err != nil {
+				return "", fmt.Errorf("%s to utf8 failed. %v", srcCharset, err)
+			}
+			src = string(tmp)
+		} else {
+			return dst, errors.New(fmt.Sprintf("unsupport srcCharset: %s", srcCharset))
+		}
+	}
+	// Do the converting from UTF-8 to <dstCharset>.
+	if dstCharset != "UTF-8" {
+		if e := getEncoding(dstCharset); e != nil {
+			tmp, err := ioutil.ReadAll(
+				transform.NewReader(bytes.NewReader([]byte(src)), e.NewEncoder()),
+			)
+			if err != nil {
+				return "", fmt.Errorf("utf to %s failed. %v", dstCharset, err)
+			}
+			dst = string(tmp)
+		} else {
+			return dst, errors.New(fmt.Sprintf("unsupport dstCharset: %s", dstCharset))
+		}
+	} else {
+		dst = src
+	}
+	return dst, nil
+}
+
+func ToUTF8(srcCharset Charset, src string) (dst string, err error) {
+	return Convert("UTF-8", srcCharset, src)
+}
+
+func UTF8To(dstCharset Charset, src string) (dst string, err error) {
+	return Convert(dstCharset, "UTF-8", src)
+}
+
+func getEncoding(charset Charset) encoding.Encoding {
+	if c, ok := charsetAlias[string(charset)]; ok {
+		charset = Charset(c)
+	}
+	if e, err := ianaindex.MIB.Encoding(string(charset)); err == nil && e != nil {
+		return e
+	}
+	return nil
+}
+
+func GbkToUtf8(s []byte) ([]byte, error) {
+	//第二个参数为“transform.Transformer”接口，simplifiedchinese.GBK.NewDecoder()包含了该接口
+	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewDecoder())
+	d, e := ioutil.ReadAll(reader)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
 }
