914981a3165c9477bbaa04684300cfccf679a489
2lovecode <liu546hao@163.com>
2024-10-11 17:21:04 +0800

    feature:leetcode 560

--- internal/app/leetcode/top100/lc560.go
+++ internal/app/leetcode/top100/lc560.go
@@ -1,6 +1,20 @@
 package top100
 
+// 560. 和为K的子数组
 func subarraySum(nums []int, k int) int {
+	preSumMap := make(map[int][]int)
+	sum := 0
+	cnt := 0
+	for i := 0; i < len(nums); i++ {
+		sum += nums[i]
+		if sum-k == 0 {
+			cnt++
+		}
+		if _, ok := preSumMap[sum-k]; ok {
+			cnt += len(preSumMap[sum-k])
+		}
+		preSumMap[sum] = append(preSumMap[sum], i)
+	}
 
-	return 0
+	return cnt
 }
--- internal/app/leetcode/top100/lc560_test.go
+++ internal/app/leetcode/top100/lc560_test.go
@@ -9,5 +9,9 @@ func TestSubarraySum(t *testing.T) {
 	convey.Convey("数组中和为k的子数组个数", t, func() {
 		convey.So(subarraySum([]int{1, 1, 1}, 2), convey.ShouldEqual, 2)
 		convey.So(subarraySum([]int{1, 2, 3}, 3), convey.ShouldEqual, 2)
+		convey.So(subarraySum([]int{1}, 0), convey.ShouldEqual, 0)
+		convey.So(subarraySum([]int{-1, -1, 1}, 0), convey.ShouldEqual, 1)
+		convey.So(subarraySum([]int{1, -1, 0}, 0), convey.ShouldEqual, 3)
+		convey.So(subarraySum([]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 0), convey.ShouldEqual, 55)
 	})
 }
