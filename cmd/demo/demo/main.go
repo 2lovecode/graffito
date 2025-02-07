bf0b65009b350711854aaf5e4e9aac2f0b4dab4e
2lovecode <liu546hao@163.com>
2025-02-07 09:12:30 +0800

    demo

--- cmd/demo/demo/main.go
+++ cmd/demo/demo/main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os/exec"
 )
 
 func main() {
-	println("hello world!")
-	println("good morning!")
-	println("hello world")
-	fmt.Println("hello world!")
-	fmt.Println("hello good!")
+	title := "Hello, World!"
+	message := "This is a notification from Go!"
+	script := fmt.Sprintf(`display notification "%s" with title "%s"`, message, title)
+
+	cmd := exec.Command("osascript", "-e", script)
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Failed to send notification:", err)
+	}
 }
