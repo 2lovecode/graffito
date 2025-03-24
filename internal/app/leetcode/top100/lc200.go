5ff643e3077a93854b97458bc9b600194ab2d441
2lovecode <liu546hao@163.com>
2025-03-24 18:39:52 +0800

    feat(leetcode):236

---
+++ internal/app/leetcode/top100/lc200.go
@@ -0,0 +1,5 @@
+package top100
+
+func numIslands(grid [][]byte) int {
+	return 0
+}
--- internal/app/leetcode/top100/lc236.go
+++ internal/app/leetcode/top100/lc236.go
@@ -9,7 +9,23 @@ package top100
  * }
  */
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
 
-	println("test")
-	return nil
+	if root == p || root == q {
+		return root
+	}
+
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+
+	if left == nil {
+		return right
+	}
+
+	if right == nil {
+		return left
+	}
+	return root
 }
