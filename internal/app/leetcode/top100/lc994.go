5da5eb319a68d6b8e301a14255756ecf4b7411a9
2lovecode <liu546hao@163.com>
2025-04-03 11:20:54 +0800

    feat(leetcode):994

--- internal/app/leetcode/top100/lc994.go
+++ internal/app/leetcode/top100/lc994.go
@@ -1,14 +1,11 @@
 package top100
 
-import "fmt"
-
 func orangesRotting(grid [][]int) int {
-	println("-----------")
 	rows := len(grid)
 	cols := len(grid[0])
 
 	bfsQueue := make([][]int, 0)
-	level := 0
+	round := 0
 
 	freshOrangeCnt := 0
 	for i := 0; i < rows; i++ {
@@ -20,37 +17,38 @@ func orangesRotting(grid [][]int) int {
 			}
 		}
 	}
-	if freshOrangeCnt == 0 {
-		return 0
-	}
-	for len(bfsQueue) > 0 {
+
+	for freshOrangeCnt > 0 && len(bfsQueue) > 0 {
 		oldL := len(bfsQueue)
+		round++
 		for i := 0; i < oldL; i++ {
-			n, m := bfsQueue[i][0], bfsQueue[i][1]
-			if grid[n][m] == 1 {
-				freshOrangeCnt--
-				grid[n][m] = 2
-			}
+			n, m := bfsQueue[0][0], bfsQueue[0][1]
+			bfsQueue = bfsQueue[1:]
 			if (n+1) < rows && grid[n+1][m] == 1 {
+				freshOrangeCnt--
+				grid[n+1][m] = 2
 				bfsQueue = append(bfsQueue, []int{n + 1, m})
 			}
 			if (n-1) >= 0 && grid[n-1][m] == 1 {
+				freshOrangeCnt--
+				grid[n-1][m] = 2
 				bfsQueue = append(bfsQueue, []int{n - 1, m})
 			}
 			if (m+1) < cols && grid[n][m+1] == 1 {
+				freshOrangeCnt--
+				grid[n][m+1] = 2
 				bfsQueue = append(bfsQueue, []int{n, m + 1})
 			}
 			if (m-1) >= 0 && grid[n][m-1] == 1 {
+				freshOrangeCnt--
+				grid[n][m-1] = 2
 				bfsQueue = append(bfsQueue, []int{n, m - 1})
 			}
 		}
-		fmt.Println(bfsQueue)
-		level++
-		bfsQueue = bfsQueue[oldL:]
 	}
 	if freshOrangeCnt > 0 {
 		return -1
 	}
 
-	return level - 1
+	return round
 }
